799e3e887708cd1c47e79257075afed5c9b0868f
Abdul Moid <adbulmoid7733@gmail.com>
2025-05-27 23:49:03 +0530

    docker-compose success

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,28 @@
-import { PrismaClient } from "@prisma/client";
-import express from "express";
+import { PrismaClient } from "@prisma/client"
+import express from "express"
 
-const app = express();
+const app = express()
 const prismaClient = new PrismaClient();
 
-app.use(express.json());
+app.get("/", async (req, res) => {
+   
+    const data = await prismaClient.user.findMany();
+    res.json({
+        data
+    })
+})
 
-app.get("/",  async(req, res) => {
-const data = await prismaClient.user.findMany(); 
-  res.json({
-    data
-  });
-});
 app.post("/", async (req, res) => {
+
     await prismaClient.user.create({
-        data : {
-         username : Math.random().toString(),
-         password : Math.random().toString(),
+        data: {
+            username: Math.random().toString(),
+            password: Math.random().toString()
         }
     })
-  res.json({
-    "message" : "post endpoint"
-  });
-});
-
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+    res.json({
+        "message": "post endpoint"
+    })
 })
+
+app.listen(3000);
